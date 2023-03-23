6781bd1a52baebee97d1e7a303b738fd7c76ac31
strpp <tommasobaldo@gmail.com>
2023-03-23 16:27:20 +0100

    implemented websocket for auth/callback mechanism

--- pharma/app.py
+++ pharma/app.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template
+from flask_session import Session
+from flask_socketio import SocketIO
 from flask_qrcode import QRcode
 import redis
+import verifier
 
 red = redis.Redis(host='localhost', port=6379, db=0)
+socketio = SocketIO()
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey' # set the secret key for sessions
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+socketio = SocketIO(app)
+verifier.init_app(app, red, socketio)
 
+Session(app)
 QRcode(app)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+   socketio.run(app, debug=True, host='0.0.0.0', port=5001)
--- pharma/verifier.py
+++ pharma/verifier.py
@@ -1,42 +1,51 @@
 from flask import jsonify, request, render_template, session, redirect, flash, Response
+from flask_socketio import SocketIO, join_room, leave_room
 from datetime import timedelta, datetime
 import json
 import uuid
 import didkit
 from web3 import Web3
 
-def init_app(app, red) :
-    app.add_url_rule('/authorize',  view_func=authorize, methods = ['GET', 'POST'], defaults={"red" : red})
-    app.add_url_rule('/verify/<stream_id>',  view_func=verify, methods = ['GET','POST'], defaults={"red" : red})
+def init_app(app, red, socketio) : 
+
+    app.add_url_rule('/authorize',  view_func=authorize, methods = ['GET', 'POST'], defaults={"red" : red, "socketio" : socketio})
+    app.add_url_rule('/verify/<stream_id>',  view_func=verify, methods = ['GET','POST'], defaults={"red" : red, "socketio" : socketio})
     app.add_url_rule('/callback/<stream_id>',  view_func=callback, methods = ['GET','POST'], defaults={"red" : red})
     return
 
-def authorize(red):
-        
+def authorize(red, socketio):
+
     #Generate stream id
     stream_id = str(uuid.uuid4().hex)
 
-    red.set(stream_id, json.dumps({'stream_id' : stream_id, 'verified' : False}))
-    # TODO: add client to web socket to listen for changes
+    @socketio.on('subscribe')
+    def subscribe(data):
+        # Add client to private channel to get further updates
+        join_room(data['socket_id'])
+        # Save socket_id on server session to make /authorize able to link socket_id and stream_id
+        socket_id = data['socket_id']
+        print(f'socket_id : {socket_id}, stream_id : {stream_id}')
+        red.set(stream_id, json.dumps({'stream_id' : stream_id, 'socket_id' : socket_id, 'verified' : False}))
 
     #Generate QR Code
-    url = f'http://192.168.1.20:5001/authorize/{stream_id}'
+    url = f'http://192.168.1.20:5001/verify/{stream_id}'
     print(url)
     return render_template('qrcode.html', url=url)
 
-def verify(red):
+def verify(stream_id, red, socketio):
 
     if request.method == 'GET':
-        #TODO
-        return 200
+        socket_id = json.loads(red.get(stream_id).decode())['socket_id']
+        print(f'{stream_id} requested auth for {socket_id}')
+        socketio.emit('stream_id', {'stream_id' : stream_id}, to=socket_id)
+        return jsonify({'stream_id' : stream_id})
     
     else:
         #TODO
         return 200
 
-def callback(red):
+def callback(stream_id, red):
     #TODO
-    return 200
-
+    return jsonify({'callback': 'success', 'stream_id' : stream_id})
 
 
