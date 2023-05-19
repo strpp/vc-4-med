89fb35c05ecd033662b802d0261362158d07e8ad
strpp <tommasobaldo@gmail.com>
2023-05-19 18:36:09 +0200

    fix

--- doctor/start.sh
+++ doctor/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo " === DOCTOR SERVER starting ==="
 echo "Running on port 5000"
-(redis-server > /dev/null 2>&1) & (cd doctor; python app.py > doctor.log 2>&1)
+(redis-server > /dev/null 2>&1) & (python app.py > doctor.log 2>&1)
