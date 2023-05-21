230b0db7e410cbeb49ba79b88fefe1648c3f1830
strpp <tommasobaldo@gmail.com>
2023-05-21 18:15:21 +0200

    fix

--- pharma/app.py
+++ pharma/app.py
@@ -46,4 +46,4 @@ def index():
 
 
 if __name__ == '__main__':
-   socketio.run(app, debug=True, host='0.0.0.0', port=5001)
+   socketio.run(app, debug=True, host='0.0.0.0', port=5001, allow_unsafe_werkzeug=True)
